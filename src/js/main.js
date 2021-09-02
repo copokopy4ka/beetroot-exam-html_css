bea01f274abb4e93a646fe66f8ec21528b106281
copokopy4ka <copokopy4ka739@gmail.com>
2021-09-02 23:22:36 +0300

    why-us, why-it-work, proposes

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,20 @@
 $( document ).ready(function() {
-    $('.js-burger-btn').on('click', function() {
+    $('.js-burger-btn').on('click', function(e) {
+        console.log(e.target);
         if($('.js-header').hasClass('active')) {
             $('.js-header').removeClass('active');
+            $('body').removeClass('menu-opened');
         } else {
             $('.js-header').addClass('active');
+            $('body').addClass('menu-opened');
         }
-    })
+    });
+
+    $(window).on('click', function(e) { 
+        let target = $( e.target ); 
+        if( target.hasClass('menu-opened') ) { 
+            $('body').removeClass('menu-opened'); 
+            $('.js-header').removeClass('active'); 
+        } 
+    });
 });
